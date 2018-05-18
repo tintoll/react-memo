cfe7926f6d5d7510d48250a8b26836ea9ccd1543
tintoll <belselios@gmail.com>
2018-05-18 15:05:22 +0900

    Header Component 추가

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Header from "components/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello Memo App
+        <Header />
       </div>
     );
   }
